4503a6dfe0d33f2fa1666f8e32fd372c9d3ca932
vickey220500 <vickey220500@gmail.com>
2025-06-10 23:09:37 +0530

    login component configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { MasterComponentComponent } from './master-component/master-component.co
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { DailyLoanComponent } from './daily-loan/daily-loan.component';
 import { DailyLoanListComponent } from './daily-loan-list/daily-loan-list.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
     {path:'',component:SidemenuComponent,
     children:[
         {path:'stepper',component:StepperComponent},

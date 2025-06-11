fa37f14708f8dfbf8d988ffb3ed9b795e92884ad
Chandru <chandruparker126@gmail.com>
2025-06-11 15:07:51 +0530

    color changes

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -14,65 +14,33 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sidemenu.component.scss'
 })
 export class SidemenuComponent {
-
-  mode: string | undefined;
-  element: any;
-  flagvalue: any;
-  cookieValue: any;
-  countryName: any;
-  valueset: any;
-  isScreenSmall = window.innerWidth < 768;
-
+  isScreenSmall = false;
+  isCollapsed = false;
 
   @Output() mobileMenuButtonClicked = new EventEmitter();
-
-  constructor(
-    private router: Router,) { }
-
-  listLang = [
-    { text: 'English', flag: 'assets/images/flags/us.jpg', lang: 'en' },
-    { text: 'Spanish', flag: 'assets/images/flags/spain.jpg', lang: 'es' },
-    { text: 'German', flag: 'assets/images/flags/germany.jpg', lang: 'de' },
-    { text: 'Italian', flag: 'assets/images/flags/italy.jpg', lang: 'it' },
-    { text: 'Russian', flag: 'assets/images/flags/russia.jpg', lang: 'ru' },
-  ];
-
   @Output() settingsButtonClicked = new EventEmitter();
 
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
+    window.addEventListener('resize', () => {
+      this.isScreenSmall = window.innerWidth < 768;
+    });
+  }
 
-    const val = this.listLang.filter(x => x.lang === this.cookieValue);
-    this.countryName = val.map(element => element.text);
-    if (val.length === 0) {
-      if (this.flagvalue === undefined) { this.valueset = 'assets/images/flags/us.jpg'; }
-    } else {
-      this.flagvalue = val.map(element => element.flag);
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+    const hostElem = document.querySelector('app-root');
+    if (hostElem) {
+      if (this.isCollapsed) {
+        hostElem.classList.add('collapsed');
+      } else {
+        hostElem.classList.remove('collapsed');
+      }
     }
-   
-      window.addEventListener('resize', () => {
-        this.isScreenSmall = window.innerWidth < 768;
-      });
-  }
-
-  setLanguage(text: string, lang: string, flag: string) {
-    this.countryName = text;
-    this.flagvalue = flag;
-    this.cookieValue = lang;
-  
-  }
-
-  toggleRightSidebar() {
-    this.settingsButtonClicked.emit();
-  }
-
- 
-  toggleMobileMenu(event: any) {
-    event.preventDefault();
-    this.mobileMenuButtonClicked.emit();
   }
 
   logout() {
- 
     this.router.navigate(['/account/login']);
   }
 }

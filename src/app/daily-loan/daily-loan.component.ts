60006e7e6453f972cbb7cbb9a02681a059ceae89
vickey220500 <vickey220500@gmail.com>
2025-06-23 20:35:07 +0530

    fixed the changes

--- src/app/daily-loan/daily-loan.component.ts
+++ src/app/daily-loan/daily-loan.component.ts
@@ -23,9 +23,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DailyLoanComponent {
   @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
-@ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
-videoStream: MediaStream | null = null;
-dailyLoan:any = [
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+  videoStream: MediaStream | null = null;
+  dailyLoan: any = [
     {
       type: 'text',
       label: 'First Name',
@@ -67,9 +67,54 @@ dailyLoan:any = [
       formControl: 'panNo',
       mandatory: false,
     },
-    { type: 'number', label: 'Pincode', formControl: 'pincode', mandatory: true },
-    { type: 'number', label: 'Borrow Amount', formControl: 'borrowAmount', mandatory: true },
-    { type: 'number', label: 'Interest', formControl: 'interest', mandatory: true },
+    {
+      type: 'number',
+      label: 'Pincode',
+      formControl: 'pincode',
+      mandatory: true,
+    },
+    {
+      type: 'number',
+      label: 'Borrow Amount',
+      formControl: 'borrowAmount',
+      mandatory: true,
+    },
+    {
+      type: 'number',
+      label: 'Interest',
+      formControl: 'interest',
+      mandatory: true,
+      changeLogic: [
+        {
+          targetField: 'interestAmount',
+          details: {
+            method: 'percentage',
+            fields: ['borrowAmount', 'interest'],
+          },
+        },
+        {
+          targetField: 'calculatedAmount',
+          details: {
+            method: 'subtraction',
+            fields: ['borrowAmount', 'interestAmount'],
+          },
+        },
+      ],
+    },
+    {
+      type: 'number',
+      label: 'Interest Amount',
+      formControl: 'interestAmount',
+      mandatory: true,
+      hidden: true,
+    },
+    {
+      type: 'number',
+      label: 'Calculated Amount',
+      formControl: 'calculatedAmount',
+      mandatory: true,
+      readOnly: true,
+    },
     {
       type: 'image',
       label: 'Profile',
@@ -77,7 +122,7 @@ dailyLoan:any = [
       mandatory: true,
     },
   ];
-dpl:any = [
+  dpl: any = [
     {
       type: 'text',
       label: 'First Name',
@@ -119,9 +164,24 @@ dpl:any = [
       formControl: 'panNo',
       mandatory: false,
     },
-    { type: 'number', label: 'Pincode', formControl: 'pincode', mandatory: true },
-    { type: 'number', label: 'Borrow Amount', formControl: 'borrowAmount', mandatory: true },
-    { type: 'number', label: 'Interest', formControl: 'interest', mandatory: true },
+    {
+      type: 'number',
+      label: 'Pincode',
+      formControl: 'pincode',
+      mandatory: true,
+    },
+    {
+      type: 'number',
+      label: 'Borrow Amount',
+      formControl: 'borrowAmount',
+      mandatory: true,
+    },
+    {
+      type: 'number',
+      label: 'Interest',
+      formControl: 'interest',
+      mandatory: true,
+    },
     {
       type: 'image',
       label: 'Profile',
@@ -129,35 +189,48 @@ dpl:any = [
       mandatory: true,
     },
   ];
-  fieldConfigure: any = []
+  fieldConfigure: any = [];
   form!: FormGroup;
   title: string = 'Daily Loan Form';
-  breadcrumbItems:any = [];
+  breadcrumbItems: any = [];
   base64Image: string | null = null;
   showImageModal: boolean = false;
   showCaptureModal: boolean = false;
   currentPage: any;
 
-  constructor(public fb: FormBuilder, public snackBar: MatSnackBar, public commonService: commonService, public utilService: UtilService, public router: Router, public route: ActivatedRoute) {
+  constructor(
+    public fb: FormBuilder,
+    public snackBar: MatSnackBar,
+    public commonService: commonService,
+    public utilService: UtilService,
+    public router: Router,
+    public route: ActivatedRoute
+  ) {
     // Initialization logic can go here
   }
   ngOnInit(): void {
     // this.route.snapshot
-    this.currentPage= this.router.url;
+    this.currentPage = this.router.url;
     if (this.currentPage === '/dailyLoan') {
       this.fieldConfigure = this.dailyLoan;
       this.title = 'Daily Loan Form';
       this.breadcrumbItems = [
-    { label: 'Daily Loan' },
-    { label: 'Daily Loan Form', active: true },
-  ]} else if (this.currentPage === '/dpl') {
-    this.fieldConfigure = this.dpl;
+        { label: 'Daily Loan' },
+        { label: 'Daily Loan Form', active: true },
+      ];
+    } else if (this.currentPage === '/dpl') {
+      this.fieldConfigure = this.dpl;
       this.title = 'DPL Form';
-      this.breadcrumbItems =[
-    { label: 'DPL' },
-    { label: 'DPL Form', active: true },
-  ]  }
-    console.log(this.currentPage,'Route Snapshot:', this.route.snapshot.pathFromRoot);
+      this.breadcrumbItems = [
+        { label: 'DPL' },
+        { label: 'DPL Form', active: true },
+      ];
+    }
+    console.log(
+      this.currentPage,
+      'Route Snapshot:',
+      this.route.snapshot.pathFromRoot
+    );
 
     this.createForm();
   }
@@ -168,7 +241,10 @@ dpl:any = [
       component.mandatory
         ? group.addControl(
             component.formControl,
-            new FormControl(component.value?component.value:'', Validators.required)
+            new FormControl(
+              component.value ? component.value : '',
+              Validators.required
+            )
           )
         : group.addControl(component.formControl, new FormControl(''));
     });
@@ -181,18 +257,20 @@ dpl:any = [
       console.log('Form Data:', this.form.value);
       let data = this.form.value;
       let url = 'insertDailyLoan';
-       this.commonService.sendData(data,url).subscribe((res: any) => {
-        console.log('POST response:', res);
-        if (res.status) {
-          this.utilService.openSwal('Success', res.message, 'success', 'OK');
-        }else {
-          this.utilService.openSwal('Error', res.message, 'error', 'OK');
+      this.commonService.sendData(data, url).subscribe(
+        (res: any) => {
+          console.log('POST response:', res);
+          if (res.status) {
+            this.utilService.openSwal('Success', res.message, 'success', 'OK');
+          } else {
+            this.utilService.openSwal('Error', res.message, 'error', 'OK');
+          }
+          console.log('POST response:', res);
+        },
+        (err: any) => {
+          this.utilService.openSwal('Error', err.message, 'error', 'OK');
         }
-      console.log('POST response:', res);
-    },(err:any)=>{
-       this.utilService.openSwal('Error', err.message, 'error', 'OK');
-    }
-    );
+      );
     } else {
       this.form.markAllAsTouched();
       this.snackBar.open('Fill mandatory Fields', 'close', { duration: 2000 });
@@ -216,48 +294,74 @@ dpl:any = [
     }
   }
 
-togglePreview(modal: string): void {
-  (this as any)[modal] = !(this as any)[modal];
-   if (modal === 'showCaptureModal' && this[modal]) {
-    this.initCamera();
-  }else if(modal === 'showCaptureModal' && !this[modal]) {
-    this.stopCamera();
-  }
-}
-
-initCamera() {
-  navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-     this.videoStream = stream;
-    if (this.video?.nativeElement) {
-      this.video.nativeElement.srcObject = stream;
+  togglePreview(modal: string): void {
+    (this as any)[modal] = !(this as any)[modal];
+    if (modal === 'showCaptureModal' && this[modal]) {
+      this.initCamera();
+    } else if (modal === 'showCaptureModal' && !this[modal]) {
+      this.stopCamera();
     }
-  }).catch(err => {
-    console.error('Camera init error:', err);
-  });
-}
+  }
 
-capturePhoto(): void {
-  const video = this.video?.nativeElement;
-  const canvas = this.canvas?.nativeElement;
-  if (!video || !canvas) return;
+  initCamera() {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        this.videoStream = stream;
+        if (this.video?.nativeElement) {
+          this.video.nativeElement.srcObject = stream;
+        }
+      })
+      .catch((err) => {
+        console.error('Camera init error:', err);
+      });
+  }
 
-  const ctx = canvas.getContext('2d');
-  if (ctx) {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    this.base64Image = canvas.toDataURL('image/png');
-    this.form.patchValue({ profilePic: this.base64Image });
-    this.form.get('profilePic')?.markAsTouched();
-    console.log('Captured Image Base64:', this.base64Image);
-    // Optional: send to backend
+  capturePhoto(): void {
+    const video = this.video?.nativeElement;
+    const canvas = this.canvas?.nativeElement;
+    if (!video || !canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.base64Image = canvas.toDataURL('image/png');
+      this.form.patchValue({ profilePic: this.base64Image });
+      this.form.get('profilePic')?.markAsTouched();
+      console.log('Captured Image Base64:', this.base64Image);
+      // Optional: send to backend
+    }
   }
-}
-stopCamera() {
-  if (this.videoStream) {
-    this.videoStream.getTracks().forEach(track => track.stop());
-    this.videoStream = null;
+  stopCamera() {
+    if (this.videoStream) {
+      this.videoStream.getTracks().forEach((track) => track.stop());
+      this.videoStream = null;
+    }
+    if (this.video?.nativeElement) {
+      this.video.nativeElement.srcObject = null;
+    }
   }
-  if (this.video?.nativeElement) {
-    this.video.nativeElement.srcObject = null;
+  onChange(event: Event, field: any): void {
+    if (field.changeLogic && field.changeLogic.length > 0) {
+      for (let index = 0; index < field.changeLogic.length; index++) {
+        const logic = field.changeLogic[index];
+        const calculatedValue = this.calculateValue(logic.details.method,logic.details.fields);
+          this.form.patchValue({ [logic.targetField]: calculatedValue });
+      }
+    }
   }
-}
+
+  calculateValue(method:any, fields: any) {
+    const values = fields.map(
+      (f: string) => this.form.get(f)?.value || 0
+    );
+    if (method === 'percentage') {
+      return (values[0] * values[1]) / 100;
+    } else if (method === 'subtraction') {
+      return values[0] - values[1];
+    }else if (method === 'addition') {
+      return values[0] + values[1];
+    }
+    return 0; // Default case, should not happen
+  }
 }

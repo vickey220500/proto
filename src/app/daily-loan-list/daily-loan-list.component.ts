0a2b04c9aa6889b0b4f3ec62f06aac254c662420
vickey220500 <vickey220500@gmail.com>
2025-07-07 19:13:29 +0530

    file download & payment changes

--- src/app/daily-loan-list/daily-loan-list.component.ts
+++ src/app/daily-loan-list/daily-loan-list.component.ts
@@ -8,13 +8,14 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { commonService } from '../services/common.service';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { UtilService } from '../services/util.service';
 @Component({
   selector: 'app-daily-loan-list',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule,  MatCardModule,
     MatTableModule,
-    MatPaginatorModule,MatIconModule,RouterModule],
+    MatPaginatorModule,MatIconModule,RouterModule, MatCheckboxModule],
   templateUrl: './daily-loan-list.component.html',
   styleUrl: './daily-loan-list.component.scss'
 })
@@ -165,7 +166,8 @@ export class DailyLoanListComponent {
   displayedColumns: any[] = []
 
  coloumnDefs: any[] = [
-  { key: 'profilePic', label: 'Profile Picture', type: 'image' },
+  { key: 'checkbox',  type: 'checkbox' },
+  { key: 'profilePic', label: 'Profile', type: 'image' },
   { key: 'firstName', label: 'First Name', type: 'text' },
   { key: 'lastName', label: 'Last Name', type: 'text' },
   { key: 'aadharNo', label: 'Aadhar No', type: 'text' },
@@ -178,45 +180,89 @@ export class DailyLoanListComponent {
   { key: 'panCardNumber', label: 'PAN No', type: 'text' },
   { key: 'pincode', label: 'Pincode', type: 'text' }
 ];
-;
+
+dlHeaderAction:any[] = [{type:'button',label: 'Pay', action: 'apiCall', url:'/dlPayment'},{type:'button',action:'route',label: 'Add', icon: 'add',route: '/dailyLoan'},]
   // dataSource:any = [ { "firstName": "Vignesh", "lastName": "", "aadharNo": 123456789012, "address": "123, Gandhi Street, Chennai", "age": 28, "borrowAmount": 50000, "interest": 7.5, "email": "abc@example.com", "mobileNumber": 9876543210, "panCardNumber": "ABCDE1234F", "pincode": 600001, "profilePic": { "data": "base64", "contentType": "image/png" } }];
-  totalRecords = 0;
   pageSize = 5;
+  selectedRows: any[] = [];
+  pagedData: any[] = [];
+  currentPage = 0;
+  headerActions: any[] = [];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private commonService: commonService) {}
+  constructor(private commonService: commonService, public utilService:UtilService) {}
 
   ngOnInit() {
-    this.loadData(0, this.pageSize);
-    
+    this.loadData();
+    this.headerActions = this.dlHeaderAction;
     this.displayedColumns = this.coloumnDefs.map(col => col.key);
     console.log('Displayed Columns:', this.displayedColumns);
 
   }
 
-  loadData(pageIndex: number, pageSize: number) {
-
-    // const allUsers: User[] = Array.from({ length: 50 }, (_, i) => ({
-    //   id: i + 1,
-    //   name: `User ${i + 1}`,
-    //   email: `user${i + 1}@example.com`,
-    //   status: i % 2 === 0 ? 'Active' : 'Inactive'
-    // }));
+  loadData() {
     this.commonService.getData('dailyLoanData').subscribe((res: any) => {
     this.dataSource = res.data;
     console.log('Data Source:', this.dataSource);
-    this.totalRecords = this.dataSource.length;
-    const start = pageIndex * pageSize;
-    const end = start + pageSize;
+    this.updatePagedData();
     });
-
-    // this.dataSource.data = allUsers.slice(start, end);
   }
 
-  onPaginateChange(event: PageEvent) {
-    this.loadData(event.pageIndex, event.pageSize);
+   onPageChange(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.updatePagedData();
   }
+
+  updatePagedData(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedData = this.dataSource.slice(startIndex, endIndex);
+  }
+
+isSelected(row: any): boolean {
+  return this.selectedRows.includes(row);
+}
+
+toggleRow(row: any): void {
+  const index = this.selectedRows.indexOf(row);
+  if (index > -1) {
+    this.selectedRows.splice(index, 1);
+  } else {
+    this.selectedRows.push(row);
+  }
+}
+
+isAllSelected(): boolean {
+  return this.selectedRows.length === this.dataSource.length;
+}
+
+isIndeterminate(): boolean {
+  return this.selectedRows.length > 0 && !this.isAllSelected();
+}
+
+toggleAll(): void {
+  if (this.isAllSelected()) {
+    this.selectedRows = [];
+  } else {
+    this.selectedRows = [...this.dataSource];
+  }
+}
+
+checkedData() {
+console.log('Selected Rows:', this.selectedRows );
+}
+
+// downloadPDF() {
+// this.utilService.downloadPdf(this.dataSource);
+// }
+
+downloadExcel() {
+const data = this.dataSource.map(({ profilePic, ...rest }:any) => rest);
+this.utilService.downloadExcel(data);
+}
+
 }
 
 
